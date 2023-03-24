cd42bdf3f3bbe5282de06c4e2453b061a7b5e249
Lucas Guimarães <78770576+lucas224112@users.noreply.github.com>
2023-03-23 21:59:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import MGE
+
+MGE.Program.init()
+
+gif = MGE.Object2D([0, 0], 0, [500, 500])
+gif.set_material(MGE.Material(MGE.Texture(MGE.Image("./img.gif"))))
+
+MGE.Program.screen.set_size(500, 500)
+MGE.Program.set_clock(120)
+
+while True:
+    if MGE.Program.event.type == 256 or MGE.keyboard("f1"):
+        sys.exit()
+
+    gif.draw_object(MGE.Program.screen)
+
+    MGE.Program.set_caption(f"Gif-MGE | FPS:{int(MGE.Program.get_fps())}")
+    MGE.Program.update(False, False)
