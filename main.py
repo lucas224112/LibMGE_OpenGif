e54f43e8d7e8473110becc20cc73880c492bf07e
Lucas Guimarães <lucasguimaraes2421@gmail.com>
2024-10-19 15:43:18 -0300

    Update script and add comments

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import MGE
+import MGE  # Import the LibMGE
 
-MGE.init()
+MGE.init()  # Initialize the library
 
-window = MGE.Window(resolution=(500, 500), flags=MGE.WindowFlag.Shown)
-window.limit_time = 60
+window = MGE.Window(resolution=(500, 500), flags=MGE.WindowFlag.Shown)  # Create a window
+window.frameRateLimit = 60  # Set frame rate limit
 
-gif = MGE.Object2D([0, 0], 0, [500, 500])
-gif.material = MGE.Material(MGE.Texture(MGE.LoadImage("./image.gif")))
+gif = MGE.Object2D((0, 0), 0, (500, 500))  # Create a 2D object
+gif.material = MGE.Material(MGE.Texture(MGE.LoadImage("./image.gif")))  # Load the GIF and assign to object
 
 while True:
-    MGE.update()
-    window.update()
+    MGE.update()  # Update logic
+    window.update()  # Update window
 
-    window.title = f"LibMGE OpenGif | FPS: {int(window.fps)}"
+    window.title = f"LibMGE OpenGif | FPS: {window.frameRate}"  # Display FPS in window title
 
-    if MGE.QuitEvent() or MGE.keyboard(MGE.KeyboardButton.F1):
+    if MGE.QuitEvent() or MGE.keyboard(MGE.KeyboardButton.F1):  # Exit if quit event or F1 key is pressed
         exit()
 
-    gif.draw_object(window)
+    gif.drawObject(window)  # Draw GIF object on window
